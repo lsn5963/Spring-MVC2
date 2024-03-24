fb39bd159598ffd770380d0752ad0f31578608a2
lsn5963 <lsn5963@naver.com>
2024-03-24 22:42:15 +0900

    feat: 검증 직접 처리 - 개발

--- validation/README.md
+++
@@ -1,2 +0,0 @@
-# Validation
-스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 검증1 - Validation
--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -41,37 +41,37 @@ public class ValidationItemControllerV1 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
-
-        //검증 오류 결과를 보관
-        Map<String, String> erros = new HashMap<>();
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes,Model model) {
+        // 검증 오류 결과를 보관
+        Map<String, String> errors = new HashMap<>();
 
         //검증 로직
         if (!StringUtils.hasText(item.getItemName())){
-            erros.put("itemName", "상품이름은 필수입니다.");
+            errors.put("itemName", "상품 이름은 필수입니다.");
         }
-        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-            erros.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다");
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000){
+            errors.put("price", "가격은 1000원에서 100만원까지 허용합니다.");
         }
-        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
-            erros.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+        if (item.getQuantity() == null || item.getQuantity() >= 9999){
+            errors.put("quantity", "수량은 최대 9999까지 허용합니다.");
         }
 
-        //특정 필드가 아닌 복합 룰 검증
-        if (item.getPrice() != null && item.getQuantity() != null) {
+        // 특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() !=null && item.getQuantity() != null){
             int resultPrice = item.getPrice() * item.getQuantity();
-            if (resultPrice < 10000){
-                erros.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
+            if(resultPrice < 10000){
+                errors.put("globalError", "가격 * 수량의 합은 10000원 이상이어야 한다. 현재 값 = "+ resultPrice);
             }
         }
 
-        //검증에 실패하면 다시 입력 폼으로 돌아가야 한다.
-        if (!erros.isEmpty()){
-            model.addAttribute("errors", erros);
+        // 검증에 실패하면 다시 입력 폼으로
+        if(!errors.isEmpty()){
+            model.addAttribute("errors", errors);
+            System.out.println("erros = " + errors);
             return "validation/v1/addForm";
         }
 
-        //성공 로직
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
